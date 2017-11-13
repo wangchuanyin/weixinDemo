3f1a434e85c30c597b9dd4e3e944badefdae51f0
wangchuanyin <wangchuanyin2000@163.com>
2017-11-13 10:41:06 +0800

    weixin demo
    To learn weixin project

---
+++ data/products-data.js
@@ -0,0 +1,8 @@
+var products = [{
+id:0,
+name:'大白菜',
+price:'5.00',
+unit:'/斤' , pic:"http://image.huanqiuxiaozhen.com/%E5%95%86%E5%93%81%E8%AF%A6%E6%83%85/2015_9_16_1442396622482#"
+}]
+
+module.export = {productList:products}
